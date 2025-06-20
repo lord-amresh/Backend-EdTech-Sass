9942c4cb7315f5762da043130d7acc6c5d09c710
lord-amresh <amresh2006@gmail.com>
2025-06-20 16:27:14 +0545

    table creation

--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,2 +1,33 @@
 
 import {Table,Column,Model,DataType} from "sequelize-typescript"
+
+@Table({
+    tableName : 'users', //
+    modelName : 'user',
+    timestamps : true
+})
+
+class User extends Model{
+    @Column({
+        type : DataType.STRING
+    })
+    declare username : string
+
+    @Column({
+        type : DataType.STRING
+    })
+    declare password : string
+
+    @Column({
+        type : DataType.STRING
+    })
+    declare email : string
+
+    @Column({
+        type : DataType.ENUM('teacher', 'institute', 'super-admin', 'student'),
+        defaultValue : 'student',
+    })
+    declare role:string
+}
+
+export default User
